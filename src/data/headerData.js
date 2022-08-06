eed575dbaa9fd242f1fd76099fd6bfcb623618cf
Mshradha <99673738+Mshradha@users.noreply.github.com>
2022-08-06 20:54:57 +0530

    s

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,9 +3,9 @@ import resume from "../assets/pdf/ShradhaR.pdf";
 
 export const headerData = {
   name: "Shradha Mishra",
-  title: "Web Developer",
+  title: "Full Stack Web Developer",
   desciption:
-    "I'm a Frontend developer based in India. I am proficient in MERN stack and looking for opportunities to learn and build. So...let's connect! and start building!",
+    "I'm a Full Stack Web developer based in India. I am proficient in MERN stack and looking for opportunities to learn and build. So...let's connect! and start building!",
   image: "",
   resumePdf: resume,
 };
